d404d7aea99766d1c9886e32bed0f9c7f1f5165e
Lea <seifried.lea@gmail.com>
2023-09-25 12:06:04 +0200

    Update MainController

--- src/main/java/com/springrefresh/refresh/controller/MainController.java
+++ src/main/java/com/springrefresh/refresh/controller/MainController.java
@@ -1,10 +1,7 @@
 package com.springrefresh.refresh.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.function.Supplier;
 
 @RestController
 public class MainController {
@@ -12,4 +9,6 @@ public class MainController {
     public String hello() {
         return "Hello World!";
     }
+
+
 }
